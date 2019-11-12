8a587dd51ad7c6d8e82d5d8075cbac8511af3dd8
Dominik Stanaszek <dominik.stanaszek@swmansion.com>
2019-11-12 16:03:26 +0100

    Partly replace path refs in mix.exs

--- mix.exs
+++ mix.exs
@@ -23,19 +23,18 @@ defmodule MembraneBinRtp.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      # TODO replace with proper gihtub refs
       {:membrane_core, path: "~/membrane-core", override: true},
       {:membrane_element_rtp, path: "/Users/dominikstanaszek/membrane-element-rtp"},
-      {:membrane_element_rtp_jitter_buffer, path: "~/membrane-element-rtp-jitter-buffer"},
+      {:membrane_element_rtp_jitter_buffer, github: "membraneframework/membrane-element-rtp-jitter-buffer"},
       {:membrane_protocol_sdp, github: "membraneframework/membrane-protocol-sdp"},
-      {:membrane_element_rtp_h264,
-       git: "https://github.com/membraneframework/membrane-element-rtp-h264", only: [:test]},
-      {:membrane_element_ffmpeg_h264, "~> 0.1", only: [:test]},
-      {:membrane_element_pcap,
-       path: "/Users/dominikstanaszek/membrane-element-pcap", only: [:test]},
-      {:membrane_element_udp, github: "membraneframework/membrane-element-udp"},
-      {:membrane_element_rtp_mpeguadio, path: "~/membrane-element-rtp-mpegaudio"},
+      {:membrane_element_rtp_mpeguadio, github: "membraneframework/membrane-element-rtp-mpegaudio"},
       {:membrane_element_sdl, github: "membraneframework/membrane-element-sdl"},
-      {:membrane_element_file, github: "membraneframework/membrane-element-file"}
+      {:membrane_element_file, github: "membraneframework/membrane-element-file"},
+      {:dialyxir, "~> 1.0.0-rc.7", only: [:dev], runtime: false},
+      {:membrane_element_rtp_h264, github: "membraneframework/membrane-element-rtp-h264", only: [:test]},
+      {:membrane_element_ffmpeg_h264, "~> 0.1", only: [:test]},
+      {:membrane_element_pcap, github: "membraneframework/membrane-element-pcap", only: [:test]}
     ]
   end
 end
